e4849f1cbe18d1923a400e85a8c96e905857a123
mkp <993646298@qq.com>
2021-01-27 10:35:47 +0800

    优化 压缩

--- blog-react/.umirc.ts
+++ blog-react/.umirc.ts
@@ -1,4 +1,6 @@
 import { defineConfig } from 'umi';
+const CompressionPlugin = require("compression-webpack-plugin");
+const productionGzipExtensions = /\.(js|css|json|txt|html|ico|svg)(\?.*)?$/i;
 
 export default defineConfig({
   history: {
@@ -92,5 +94,19 @@ export default defineConfig({
   ],
   dynamicImport: {
     loading: '@/Loading',
+  },
+  // 开启gzip压缩
+  chainWebpack(memo){
+    memo.plugin('CompressionPlugin').use(new CompressionPlugin({
+      filename: "[path].gz[query]",
+      algorithm: "gzip",
+      test: productionGzipExtensions,
+      // 只处理大于xx字节 的文件，默认：0
+      threshold: 10240,
+      // 示例：一个1024b大小的文件，压缩后大小为768b，minRatio : 0.75
+      minRatio: 0.8, // 默认: 0.8
+      // 是否删除源文件，默认: false
+      deleteOriginalAssets: false
+    }));
   }
 });
